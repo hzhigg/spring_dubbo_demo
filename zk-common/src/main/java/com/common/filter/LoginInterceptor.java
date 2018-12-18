0dea30b1f6e3530df0aecf8dad58c7b59ed88682
huangzhi <huangz@g2l-service.com>
2018-12-18 18:10:46 +0800

    调整依赖

--- zk-common/src/main/java/com/common/filter/LoginInterceptor.java
+++ zk-common/src/main/java/com/common/filter/LoginInterceptor.java
@@ -1,4 +1,4 @@
-package com.common.filter;
+/*package com.common.filter;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,11 +11,11 @@ import org.springframework.web.servlet.ModelAndView;
 
 import com.facade.entity.User;
 
-/**
+*//**
  * 登入拦截
  * @author huangzhi
  * 2018年9月14日
- */
+ *//*
 public class LoginInterceptor implements HandlerInterceptor{
 	
 	// 配置系统级别无需校验的URI列表
@@ -32,29 +32,29 @@ public class LoginInterceptor implements HandlerInterceptor{
 	@Override
 	public void afterCompletion(HttpServletRequest arg0, HttpServletResponse arg1, Object arg2, Exception arg3)
 			throws Exception {
-		/**
+		*//**
 		 *整个请求处理完毕回调方法，即在视图渲染完毕时回调，如性能监控中我们可以在此记录结束时
 		 *间并输出消耗时间，还可以进行一些资源清理，类似于try-catch-finally中的finally，但仅调用处理器执行链中
-		 */
+		 *//*
 		System.out.println("=======销毁过滤器=========");
 	}
 
 	@Override
 	public void postHandle(HttpServletRequest arg0, HttpServletResponse arg1, Object arg2, ModelAndView arg3)
 			throws Exception {
-		/**
+		*//**
 		 * 后处理回调方法，实现处理器的后处理（但在渲染视图之前），此时我们可以通过modelAndView（模型和视图对象）对模型数据进行处理或对视图进行处理，modelAndView也可能为null。
-		 */
+		 *//*
 		System.out.println("=======化过滤器执行?=========");
 	}
 
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object arg2) throws Exception {
-		/**
+		*//**
 		 * 预处理回调方法，实现处理器的预处理（如登录检查），第三个参数为响应的处理器（如我们上一章的Controller实现）；
 		 *   返回值：true表示继续流程（如调用下一个拦截器或处理器）；
 		 *   false表示流程中断（如登录检查失败），不会继续调用其他的拦截器或处理器，此时我们需要通过response来产生响应；
-		 */
+		 *//*
 		System.out.println("=======初始化过滤器=========");
 		Object user=getSession(request);
 		String url = request.getRequestURI(); // 当前请求的url
@@ -102,3 +102,4 @@ public class LoginInterceptor implements HandlerInterceptor{
 	}
 
 }
+*/
